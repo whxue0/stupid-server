cbfbe71dc34d2bfa3a94b242df73fd77d35c101d
whxue0 <whxue0@163.com>
2021-03-12 17:53:52 +0800

    cors error

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
-
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  next()
+})
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
